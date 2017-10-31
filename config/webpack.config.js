add333f3b53d98d4846ed9db7d7e9e13480d7b67
ChugunovRoman <Zebs-BMK@yandex.ru>
2017-10-31 10:34:01 +0300

    add webpack

---
+++ config/webpack.config.js
@@ -0,0 +1,80 @@
+const nodeExternals = require('webpack-node-externals'); // for exclude node core modules in bundle file
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin'); // Optimization plugin
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+const path = require('path');
+
+
+// project folder
+const rootFolder = path.resolve(__dirname, '..');
+
+// for ext.
+const regExps = {
+    ts: /\.ts$/
+};
+
+const config = {
+
+    context: `${rootFolder}`,
+
+    entry: {
+        // main file
+        index: path.join(rootFolder, '/src/index.ts')
+    },
+
+    output: {
+        // output path
+        path: path.join(rootFolder, '/build/'),
+
+        // file name pattern for entry scripts
+        filename: '[name].js'
+    },
+
+    target: 'node',
+
+    externals: [
+        nodeExternals()
+    ],
+
+    module: {
+        rules: [
+            // rule for TypeScript
+            {
+                test: regExps.ts,
+                use: [
+                    {
+                        loader: 'ts-loader',
+                        options: {
+                            transpileOnly: true 
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+
+    devtool: 'source-map',
+
+    plugins: [
+        new ForkTsCheckerWebpackPlugin(),
+    
+        new UglifyJSPlugin({
+            parallel: 4,
+            cache: true,
+            uglifyOptions: {
+                ie8: false,
+                ecma: 8,
+                warnings: false,
+                compress: {
+                    drop_console: true
+                }
+            }
+        })
+    ]
+};
+
+module.exports = config;
--- src/cmd.ts
+++ src/cmd.ts
@@ -1,16 +1,15 @@
 process.stdin.setEncoding('utf8');
 
+// Read stdin for inputing cmd
 process.stdin.on('readable', () => {
     let input = process.stdin.read();
     
     if (input !== null) {
         let cmd = input.toString().trim();
 
-        console.log('cmd: ', cmd);
         if (cmd === 'exit') {
+            console.log('Close server.');
             process.exit(0);
-        } else if (cmd === 'require') {
-            console.log(require);
         }
     }
 });
